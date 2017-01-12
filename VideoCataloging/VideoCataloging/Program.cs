e7192d236606dcd3080b363cde23b93796918566
michaelbadaracca <michael.badaracca@gmail.com>
2017-01-11 17:36:21 -0700

    Added initial brainstorming notes

--- VideoCataloging/VideoCataloging/Program.cs
+++ VideoCataloging/VideoCataloging/Program.cs
@@ -10,6 +10,68 @@ namespace VideoCataloging
     {
         static void Main(string[] args)
         {
+
+            //CreateFoldersFromSpreadsheet
+            //CreateLabelsFromSpreadsheet
+            //CreatesLabelesFromFolder
+            //CreateSpreadsheetFromFolder
+            //RipIsoFromDisc
+            //ConvertIsoToMp4
+            //GetVideoLength
+            //GetVideoNameFromDvd
+
+
+            //VideoMetadata
+                //VideoId
+                //WrittenName
+                //RefinedName
+                //Length
+                //Author
+                //Organization
+                //PeopleInvolved (list)
+                //Comments
+                //SeriesLength (int)
+                //SeriesNum
+                //VhsPicture (path)
+                //VhsRecordingQuality (enum)
+                
+            
+            //Conversion State
+                //ConvertedVhsToDvd
+                //ConvertedDvdToIso
+                //ConvertedIsoToMp4
+                //DvdMatchedWithVhs
+                //UniqueLabelCreated
+                //VhsLabeled
+                //DvdLabeled
+               
+            //Resources
+                //ffmpeg
+                //handbrake
+                //opensource spreadsheet dll
+                //something to convert dvd to iso
+
+            //Desired Operation
+                //Digitize Video
+                    //Start app
+                    //Give command to put dvds on computer
+                    //DVD to computer flow
+                        //Ask for top directory for DVD (use filepicker)
+                        //Ask for name of DVD
+                            //Save to database (either excel spreasheet or actual database)
+                        //Open DVD Drive
+                        //once DVD has been inserted
+                            //Create Folder in top directory with DVD Name
+                            //Begin creating ISO with librarys
+                                    //High Quality
+                                    //Get rid of interlacing
+                        //Once dvd is finished give a message saying complete (and sound maybe)
+                        //Repeat prompt for next dvd name
+                    //Have the ability to exit to main menu
+    
+                //Batch convert to mp4
+                //
+                
         }
     }
 }
